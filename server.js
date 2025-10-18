ddd398bb19548fb092f0898fa70784b646753448
Kisang Alex <Alikisang9@gmail.com>
2025-10-18 23:49:18 +0300

    static middleware

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.use(
 
 app.use(express.json({ limit: '10mb' })); 
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.static("static"));
 app.use("/uploads", express.static("uploads"));
 
 app.use("/api/auth", authRoutes); 
